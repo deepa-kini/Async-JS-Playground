a0fb52568da435055592c91c68edc8c37cd98d08
deepa-kini-lux <deepa.kini@luxeveryday.com>
2022-02-10 10:48:02 +1100

    Nested callbacks

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 console.log('before');
-getUser(1, user => console.log('user', user));
+getUser(1, user => {
+  console.log('User', user);
+  getRepositories(user.name, (repos) => {
+    console.log('repos', repos);
+  });
+});
 console.log('after');
 
 
@@ -7,5 +12,12 @@ function getUser(id, callback) {
   setTimeout(() => {
     console.log('Connecting to DB...');
     callback({ id, name: 'Dee' });
+  }, 2000);
+}
+
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    console.log('Fetching Repos for username')
+    callback(['r1', 'r2']);
   }, 2000);
 }
