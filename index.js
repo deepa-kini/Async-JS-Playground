bf52ac74088d2c8f1bad7be20529eaeb22dddd5c
deepa-kini-lux <deepa.kini@luxeveryday.com>
2022-02-14 10:55:43 +1100

    Resolving callback hell using promises (chaining)

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 console.log('before');
-getUser(1, displayUserDetails);
-
-function displayUserDetails(user) {
-  console.log('User', user);
-  getRepositories(user.name, displayRepositories);
-}
+getUser(1)
+  .then(user => getRepositories(user.name))
+  .then(repos => displayRepositories(repos))
+  .catch(err => console.log('Error', err.message));
 
 function displayRepositories(repos) {
   console.log('repos', repos);
@@ -12,16 +10,20 @@ function displayRepositories(repos) {
 console.log('after');
 
 
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log('Connecting to DB...');
-    callback({ id, name: 'Dee' });
-  }, 2000);
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Connecting to DB...');
+      resolve({ id, name: 'Dee' });
+    }, 2000);
+  })
 }
 
-function getRepositories(username, callback) {
-  setTimeout(() => {
-    console.log('Fetching Repos for username')
-    callback(['r1', 'r2']);
-  }, 2000);
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Fetching Repos for username')
+      resolve(['r1', 'r2']);
+    }, 2000);
+  })
 }
