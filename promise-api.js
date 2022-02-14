6cd8c9da26d2ce9a57daba54fc0f54e8cfa47961
deepa-kini-lux <deepa.kini@luxeveryday.com>
2022-02-14 13:43:11 +1100

    Async await example

--- promise-api.js
+++ promise-api.js
@@ -1,4 +1,16 @@
-const p = Promise.resolve(1);
-p.then(res => console.log(res));
-const q = Promise.reject(new Error('e'));
-q.catch(err => console.log(err.message));
+const p1 = new Promise(resolve => {
+  setTimeout(() => {
+    console.log('Async 1');
+    resolve(1);
+  }, 2000);
+});
+const p2 = new Promise(resolve => {
+  setTimeout(() => {
+    console.log('Async 2');
+    resolve(2);
+  }, 3000);
+});
+
+Promise.all([p1, p2])
+  .then(r => console.log('Result', r))
+  .catch(e => console.log(e.message));
